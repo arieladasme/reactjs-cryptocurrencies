ab6900a064dcea6cef860356aec9d0bd34f1096c
Ariel <ariel.wm1@gmail.com>
2020-07-14 20:41:02 -0400

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React from 'react'
+import styled from '@emotion/styled'
+import image from './cryptocurrencies.png'
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700px;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`
 
 function App() {
-  return <h1>Cotizador de Criptomonedas</h1>;
+  return (
+    <Container>
+      <div>
+        <Image src={image} alt="crypto image" />
+      </div>
+      <div>
+        <Heading>Cotiza Criptomonedas al instante</Heading>
+      </div>
+    </Container>
+  )
 }
 
-export default App;
+export default App
