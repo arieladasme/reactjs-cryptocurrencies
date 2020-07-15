6365e1bae3a3b762d8b65e47ddb35e47fd3216c9
Ariel <ariel.wm1@gmail.com>
2020-07-15 13:49:32 -0400

    Reading selection from custom hook and adding styled component

--- src/hooks/useCoin.js
+++ src/hooks/useCoin.js
@@ -1,4 +1,25 @@
 import React, { Fragment, useState } from 'react'
+import styled from '@emotion/styled'
+
+const LAbel = styled.label`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`
+
+const SelectOptions = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  -webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.1rem;
+`
 
 const useCoin = (label, initialState, optionsCoins) => {
   // State of our custom hook
@@ -6,15 +27,15 @@ const useCoin = (label, initialState, optionsCoins) => {
 
   const Select = () => (
     <Fragment>
-      <label>{label}</label>
-      <select>
+      <LAbel>{label}</LAbel>
+      <SelectOptions onChange={e => updateState(e.target.value)} value={state}>
         <option value="">- Seleccione -</option>
         {optionsCoins.map(option => (
           <option key={option.code} value={option.code}>
             {option.name}
           </option>
         ))}
-      </select>
+      </SelectOptions>
     </Fragment>
   )
 
